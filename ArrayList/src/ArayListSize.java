70ffd3bdf5050609bc72f3b2a9276909f6ed7783
LenovaPC <LenovaPC@DESKTOP-9FDNQSH>
2020-08-20 11:17:34 +0530

    second commit

---
+++ ArrayList/src/ArayListSize.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class ArayListSize {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+	      numbers.add(1);
+	      numbers.add(7);
+	      numbers.add(5);
+	      numbers.add(6);
+	      System.out.println("Number of elements in ArrayList: "+numbers.size());
+	   }
+
+	}
+
+
